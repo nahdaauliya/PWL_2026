fd655225d5c37c5688bb73c562aef44cba593e8e
Nahda Auliya Raudhatunnisa <nahdaaulia653@gmail.com>
2026-03-03 17:37:08 +0700

    Query Builder

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\PhotoController;
 
 /*
@@ -66,3 +67,6 @@ Route::get('/level', [LevelController::class, 'index']);
 // Route::get('/greeting', function () {
 // 	return view('blog.hello', ['name' => 'Nahda']);
 // });
+
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
