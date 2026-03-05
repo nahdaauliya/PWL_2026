bbd2a35c6e5832a256562673a1a5f7221c7e6330
Nahda Auliya Raudhatunnisa <nahdaaulia653@gmail.com>
2026-03-06 01:52:43 +0700

    fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,12 +10,12 @@ class UserController extends Controller
 {
     public function index(){
         $data = [
-            // 'username' => 'customer',
-            'nama' => 'Pelanggan Pertama',
-            // 'password' => Hash::make('12345'),
-            // 'level_id' => 3
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data);
+        UserModel::create($data);
 
         $user = UserModel::all();
         return view('user', ['data' => $user]);
