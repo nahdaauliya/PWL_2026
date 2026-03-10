97caaaf064a62261fafe2591ddabaa2ac7a729fe
Nahda Auliya Raudhatunnisa <nahdaaulia653@gmail.com>
2026-03-10 21:58:12 +0700

    Attribute Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,9 +21,31 @@ class UserController extends Controller
         // return view('user', ['data' => $user]);
 
         // $user = UserModel::firstwhere('level_id', 1);
-        $user = UserModel::findOr(20, ['username', 'nama'], function(){
-            abort(404);
-        });
-        return view('user', ['data' => $user]);
+        // $user = UserModel::findOr(20, ['username', 'nama'], function(){
+        //     abort(404);
+        // });
+        // $user = UserModel::findOrFail(1);
+
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // $user = UserModel::where('level_id', 2)->count();
+        // dd($user);
+
+        $user = UserModel::create(
+            [
+                'username' => 'manager11',
+                'nama' => 'Manager11',
+                'password' => Hash::make('12345'),
+                'level_id' => 2,
+            ],
+        );
+        $user->username = 'manager12';
+
+        $user->save();
+
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged('username', 'level_id');
+        $user->wasChanged('nama');
+        dd($user->wasChanged(['nama', 'username']));
     }
 }
